08f94a7cecc43e0466dcd14658000eab87eae716
Guillem Garcia <guillemgarciagomez@gmail.com>
2021-01-21 13:06:17 +0100

    feat: added TS to reports.schema

---
+++ server/models/reports.models/reports.schema.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IReport extends Document {
+  title: string,
+  description: string,
+  tags: [string?],
+  steps: [string?],
+  images: [string?]
+}
+
+const ReportSchema: Schema = new Schema({
+  title: { type: String, required: true},
+  description: { type: String, required: true },
+  tags: { type: [String], required: true },
+  steps: { type: [String], required: false },
+  images: { type: [String], required: false}
+}, { autoCreate: true });
+
+export default mongoose.model<IReport>('Report', ReportSchema);
