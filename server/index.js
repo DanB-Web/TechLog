241ee6b1d5611acf6cb2e1f43172a2121fffada6
Guillem Garcia <guillemgarciagomez@gmail.com>
2021-01-21 12:19:25 +0100

    js-to-ts-backend

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ const cors = require('cors');
 const router = require('./router');
 require('dotenv').config();
 
-//Config
+// Config
 const app = express();
 const PORT = process.env.PORT || 3002;
 const HOST = process.env.HOST || `a secret host`;
 
 
-//Middleware
+// Middleware
 app.use(
   express.json(),
   cors(),
   router
 );
 
-//Start server
-app.listen(PORT, ()=> {
+// Start server
+app.listen(PORT, () => {
   console.log(`Server running on ${HOST}:${PORT} 🚀🚀🚀`);
 });
