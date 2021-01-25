2e2e8bb002836ebb3820f3645cea0e2addebbb40
Dalton Klein <deklein@live.com>
2021-01-25 03:08:16 -0600

    Feat: Added testing to Nav Component

--- client/techlog/src/Components/Nav/tests/Navbar.test.tsx
+++ client/techlog/src/Components/Nav/tests/Navbar.test.tsx
@@ -1,10 +1,78 @@
 import React from 'react'
-import { render , screen } from '@testing-library/react';
+import { render , screen, fireEvent, cleanup } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Navbar from '../Navbar';
+import Authorised from '../../App/Authorised';
+
 
 describe('SearchTags Tests', () => {
-  it('Should render the Navbar', async () => {    
-   render(<Navbar />);
-    expect(await screen.findByText(/DanB/)).toBeInTheDocument();
+  beforeEach(async () => {
+    cleanup()
   });
+
+  it('Should render the Navbar', async () => {    
+    render(
+      <MemoryRouter >
+        <Navbar 
+          admin={true} 
+          logout={()=> {}} 
+          mode={'test'} 
+          authorised={true} 
+          toggleMode={()=> {}} 
+        />
+      </MemoryRouter>
+    );
+    expect(await screen.findByText(/New/)).toBeInTheDocument();
+  });
+
+  it('Should take user to New page on link press', async () => {
+   
+    const { getByTestId, debug } = render(
+      <MemoryRouter>
+        <Authorised/>
+      </MemoryRouter>
+    );
+    const editButton = getByTestId('edit-button')
+    fireEvent.click(editButton)
+    //debug();
+    await screen.findByText(/REPORT ID:/)
+    const newButton = getByTestId('new-button')
+    fireEvent.click(newButton)
+    await screen.findByText(/Report Title/)
+    
+  })
+
+  it('Should take user to Edit page on link press', async () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Authorised/>
+      </MemoryRouter>
+    );
+    const editButton = getByTestId('edit-button')
+    fireEvent.click(editButton)
+    await screen.findByText(/REPORT ID:/)
+
+  })
+
+  it('Should take user to New page on link press', async () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Authorised/>
+      </MemoryRouter>
+    );
+    const newButton = getByTestId('new-button')
+    fireEvent.click(newButton)
+    await screen.findByText(/Report Title/)
+  })
+
+  it('Should take user to New page on link press', async () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Authorised/>
+      </MemoryRouter>
+    );
+    const logoutButton = getByTestId('logout-button')
+    fireEvent.click(logoutButton)
+    await screen.findByText(/Please log in.../)
+  })
 })
